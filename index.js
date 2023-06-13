7e39b7ee45ad5ec3177756503abe3a161da0450d
ShamimHossain1 <beastb697@gmail.com>
2023-06-13 19:35:48 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,9 +8,43 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yervfsu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+
+        const usersCollection = client.db('ass12').collection('users');
+        const courseCollection = client.db('ass12').collection('course');
+        const cartCollection = client.db('ass12').collection('carts');
+        const paymentCollection = client.db('ass12').collection('payment');
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
+    }
+}
+//   run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    })
+})
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 * Firebase live: https://ass-12-d2ded.web.app/
-* Clint Github: https://github.com/programming-hero-web-course1/b712-summer-camp-client-side-FAHIMXGG
+* Clint Github: https://github.com/programming-hero-web-course1/b712-summer-camp-client-side-ShamimHossain1
 * API: https://ass-12-server-rose.vercel.app
-* API Github: https://github.com/programming-hero-web-course1/b7a12-summer-camp-server_side-FAHIMXGG
+* API Github: https://github.com/programming-hero-web-course1/b7a12-summer-camp-server_side-ShamimHossain1
 * API from: Varcel 
