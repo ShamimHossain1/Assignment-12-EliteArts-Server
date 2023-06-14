336ad5f44140dedaeaaeb6578aec583e0c3c9ef1
ShamimHossain1 <beastb697@gmail.com>
2023-06-14 22:50:07 +0600

    payment api

--- index.js
+++ index.js
@@ -200,6 +200,16 @@ async function run() {
         clientSecret: paymentIntent.client_secret
       })
     })
+// payments api
+app.post('/payments', verifyJWT, async (req, res) => {
+  const payment = req.body;
+  const insertResult = await paymentCollection.insertOne(payment);
+
+  const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+  const deleteResult = await cartCollection.deleteMany(query)
+
+  res.send({ insertResult, deleteResult });
+})
 
 
 
